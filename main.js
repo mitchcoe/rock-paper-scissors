39c6910ced0583538f9c57abcae407314e14432a
Mitchell Coe <mitchellcoe@Mitchells-MacBook.local>
2018-01-14 23:00:28 -0600

    Added buttons to index.html for later use. Re-did code on main.js with DOM manipulation methods. They only update what is shown on the page when the game is over or paused, will need to fix this when re-working code to integrate buttons for the new UI

--- main.js
+++ main.js
@@ -1,81 +1,104 @@
+//this file currently works, but only in console.
+//only the restart button works, DOM uonly pdates after game is over or paused
+//issue on line 18
+//will replace console.log() methods with DOM methods and a UI with working buttons
+
 const arr = ["rock", "paper", "scissors"];
+const screenResult = document.getElementById('result');
+const screenScore = document.getElementById('score');
 
 //Picks from the choices in arr and stores it in computerChoice
 function computerPlay() {
     let computerChoice = arr[Math.floor(Math.random() * Math.floor(3))];
+    console.log(`This is computer choice: ${computerChoice}`);
     return computerChoice;
 }
 
 //Takes input from user and stores it in playerChoice
 function humanPlay() {
     let playerChoice = prompt("Rock, paper, or scissors?", '').toLowerCase();
-    while(!arr.includes(playerChoice)) {
-        if(playerChoice === null) {
-            break;
-        }
+//toLowerCase breaks game if null is value, but also pauses game as a result
+    while(!arr.includes(playerChoice) && playerChoice !== null) {
         playerChoice = prompt("Please try again", '').toLowerCase();
-        if(arr.includes(playerChoice)) {
-            return playerChoice;
-        }
+        //if(playerChoice === null) break;;
+        if(arr.includes(playerChoice)) return playerChoice;
     }
     return playerChoice;
 }
+
 
 //Plays a single round
 function playRound(playerChoice, computerChoice) {
     if(playerChoice !== computerChoice) {
         if(playerChoice == "rock" && computerChoice == "scissors") {
-            result = `You win! ${playerChoice} beats ${computerChoice}!`;
+            screenResult.innerHTML = `You win! ${playerChoice} beats ${computerChoice}!`;
         } else if(playerChoice == "paper" && computerChoice == "rock") {
-            result = `You win! ${playerChoice} beats ${computerChoice}!`;
+            screenResult.innerHTML = `You win! ${playerChoice} beats ${computerChoice}!`;
         } else if (playerChoice == "scissors" && computerChoice == "paper") {
-            result = `You win! ${playerChoice} beats ${computerChoice}!`;
+            screenResult.innerHTML = `You win! ${playerChoice} beats ${computerChoice}!`;
         } else {
-            result = `You lose! ${computerChoice} beats ${playerChoice}!`;
+            screenResult.innerHTML = `You lose! ${computerChoice} beats ${playerChoice}!`;
         }
     } else {
-        result = "Tie!";
+        screenResult.innerHTML = `Tie! Both players chose ${playerChoice}`;
     }
-    return result;
+    return screenResult;//.innerHTML;
 }
-//important to pass humanPlay and computerPlay functions in as arguments for playRound,
-//otherwise, playRound cannot access playerChoice and computerChoice
+
+function victory() {
+    const win = "You won the game! Congratulations!";
+    console.log(screenResult.innerHTML);
+    console.log(screenScore.innerHTML);
+    return screenResult.innerHTML = win; 
+}
+
+function defeat() {
+    const lose = "You lost the game! Sorry!";
+    console.log(screenResult.innerHTML);
+    console.log(screenScore.innerHTML);
+    return screenResult.innerHTML = lose;
+}
+
+/*
+function update() {
+    const screenResult = document.getElementById('result');
+    const screenScore = document.getElementById('score'); 
+}
+*/
+//no idea where o put this or why it doesnt work
+//everthing works now except the playerChoice.toLowerCase == null issue
+//and the html not updating between prompts.
 
 function game() {
-    let win = "You won the game! Congratulations!";
-    let lose = "You lost the game! Sorry!";
-    for(let i = 0, playerScore=0, computerScore=0; playerScore <= 5 && computerScore <= 5; i++) {
-        let score = `You: ${playerScore} | Computer: ${computerScore}`;
-        console.log(score);
+    let playerScore = 0,
+        computerScore = 0;
+        let total = `You: ${playerScore} | Computer: ${computerScore}`;
+    for(let i = 0; playerScore <= 5 && computerScore <= 5; i++) {
         playRound(humanPlay(), computerPlay());
-        if(result.includes("win")) {
-            ++playerScore;
-            console.log(result);
-        } else if(result.includes("lose")) {
-            ++computerScore;
-            console.log(result);
-        } else if(result.includes("Tie")) {
-            playerScore = playerScore;
-            computerScore = computerScore;
-            console.log(result);
+        if(screenResult.textContent.includes("win")) {
+            total = `You: ${++playerScore} | Computer: ${computerScore}`;
+            screenScore.innerHTML = total;
+        } else if(screenResult.textContent.includes("lose")) {
+            total = `You: ${playerScore} | Computer: ${++computerScore}`;
+            screenScore.innerHTML = total;
+        } else if(screenResult.textContent.includes("Tie")) {
+            total = `You: ${playerScore} | Computer: ${computerScore}`;
+            screenScore.innerHTML = total;
         }
         
         if(playerScore == 5) {
-            let final = win;
-            console.log(final);
-            alert(final);
-            return final;
+            victory();
+            return;
         } else if(computerScore == 5) {
-            let final = lose;
-            console.log(final)
-            alert(final);
-            return final;
+            defeat();
+            return;
         }
+        console.log(screenResult.innerHTML);
+        console.log(total);
     }
 }
 
-function restart() {
-    game();
-}
-
+const restart = document.getElementById('restart');
+restart.addEventListener('click', game);
+//window.addEventListener('input', update());
 game();
